223a541b93902f9161305f5c83fc55f5d020690d
Aditya Rohilla <adityarohilla@Adityas-MacBook-Pro.local>
2019-07-18 19:19:01 -0500

    Directly sent to MainActivity if user is logged in

--- TinderClone-master/app/src/main/java/com/subcrowd/app/ChooseLoginRegistrationActivity.java
+++ TinderClone-master/app/src/main/java/com/subcrowd/app/ChooseLoginRegistrationActivity.java
@@ -3,18 +3,42 @@ package com.subcrowd.app;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
     private Button mLogin, mRegister;
+    private FirebaseAuth mAuth;
+    public String TAG;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registration);
+        TAG = "chooseLoginRegistration";
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth != null){
+            final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user !=null){
+                Intent intent = new Intent(ChooseLoginRegistrationActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+            else {
+                Log.d(TAG, "user is null");
+            }
+        }
+
 
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
